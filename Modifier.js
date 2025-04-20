786a481cc5f60909b85d32665cdbed904eb1ef9f
pranavperumal <pranavpperumal@gmail.com>
2025-04-20 00:48:47 -0400

    no longer remove element, only remove the classes

--- Modifier.js
+++ Modifier.js
@@ -1,8 +1,17 @@
-const difficulty = document.querySelector('.text-difficulty-hard, .text-difficulty-easy, .text-difficulty-medium');
+const difficultyElement = document.querySelector('.text-difficulty-hard, .text-difficulty-easy, .text-difficulty-medium');
 
-if (difficulty) {
-    console.log(`This LeetCode problem is: ${difficulty.innerText}`)
-    difficulty.remove();
+if (difficultyElement) {
+    const diff = difficultyElement.innerText;
+    console.log(`This LeetCode problem is: ${diff}`);
+
+    //Remove all the special styling that can be hint towards difficulty level
+    difficultyElement.classList.forEach(className => {
+        if (className.toLowerCase().includes(diff.toLowerCase())) {
+            difficultyElement.classList.remove(className);
+        }
+    })
+
+    difficultyElement.innerText = "Difficulty: Hidden";
 }
 else {
     alert("Couldn't find the difficulty display!")
