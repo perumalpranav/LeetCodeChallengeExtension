7b944260d51461b804f77e6ac9d258fc516284a0
pranavperumal <pranavpperumal@gmail.com>
2025-04-19 21:51:19 -0400

    Fixed difficulty display

--- Modifier.js
+++ Modifier.js
@@ -1,7 +1,7 @@
 const difficulty = document.querySelector('.text-difficulty-hard, .text-difficulty-easy, .text-difficulty-medium');
 
 if (difficulty) {
-    console.log(`This LeetCode problem is: ${difficulty.innerHTMl}`)
+    console.log(`This LeetCode problem is: ${difficulty.innerText}`)
     difficulty.remove();
 }
 else {
