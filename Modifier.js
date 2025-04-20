f5ebb724de5431cce89179204387aadcbab9d0b2
pranavperumal <pranavpperumal@gmail.com>
2025-04-20 01:41:41 -0400

    Mutation Observer works in theory BUT causes infinite loop

--- Modifier.js
+++ Modifier.js
@@ -1,19 +1,30 @@
-const difficultyElement = document.querySelector('.text-difficulty-hard, .text-difficulty-easy, .text-difficulty-medium');
+const difficultyElements = document.querySelectorAll('.text-difficulty-hard, .text-difficulty-easy, .text-difficulty-medium');
 
-if (difficultyElement) {
-    const diff = difficultyElement.innerText;
+const modifier = (elem) => {
+    const diff = elem.innerText;
     console.log(`This LeetCode problem is: ${diff}`);
 
     //Remove all the special styling that can be hint towards difficulty level
-    [...difficultyElement.classList].forEach(className => {
-        console.log(`Class: ${className.toLowerCase()} includes ${diff.toLowerCase()}: ${className.toLowerCase().includes(diff.toLowerCase())}`);
+    [...elem.classList].forEach(className => {
         if (className.toLowerCase().includes(diff.toLowerCase())) {
-            difficultyElement.classList.remove(className);
+            elem.classList.remove(className);
         }
     })
 
-    difficultyElement.innerText = "Difficulty: Hidden";
-}
-else {
+    //Change the text of the display
+    elem.innerText = "Difficulty: Hidden";
+};
+
+difficultyElements.forEach(elem => {
+    //Perform initial modification
+    modifier(elem);
+
+    //Set up observer to modify again if there are dynamic updates
+    new MutationObserver(() => modifier(elem))
+        .observe(elem, { attributes: true, attributeFilter: ['class'] });
+});
+
+if (difficultyElements.length === 0) {
     alert("Couldn't find the difficulty display!")
 }
+
