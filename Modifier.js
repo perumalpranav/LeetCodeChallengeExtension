ded11706b9179a7aba3951100e3b75346f5c48d4
pranavperumal <pranavpperumal@gmail.com>
2025-04-20 02:41:08 -0400

    back to the basics, remove from DOM

--- Modifier.js
+++ Modifier.js
@@ -1,40 +1,11 @@
-function hideDifficulties() {
-    //Select all difficulty elements
-    const difficultyElements = document.querySelectorAll('.text-difficulty-hard, .text-difficulty-easy, .text-difficulty-medium');
-    
-    if (difficultyElements.length !== 0) {
-        difficultyElements.forEach(elem => {
-            const diff = elem.innerText;
-            //Log the difficulty
-            console.log(`This LeetCode problem is: ${diff}`);
-            
-            //Remove all the special styling that can be hint towards difficulty level
-            [...elem.classList].forEach(className => {
-                if (className.toLowerCase().includes(diff.toLowerCase())) {
-                    elem.classList.remove(className);
-                }
-            });
-            
-            //Change the text of the display
-            elem.innerText = "Difficulty: Hidden";
-        });
-    }
+const difficulty = document.querySelector('.text-difficulty-hard, .text-difficulty-easy, .text-difficulty-medium');
+
+if (difficulty) {
+    console.log(`This LeetCode problem is: ${difficulty.innerText}`);
+    difficulty.remove();
+}
+else {
+    alert("Couldn't find the difficulty display!")
 }
 
-hideDifficulties();
 
-//Attach observer to the document body
-let timeout;
-const observer = new MutationObserver(() => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-        hideDifficulties();
-    }, 100);
-});
-
-/*
-Need to find more precise parent element that persists when randomizing to prevent unecesary calls
-const description = document.querySelector('.flex .h-full .w-full .flex-col');
-*/
-
-observer.observe(document.body, { childList: true, subtree: true });
